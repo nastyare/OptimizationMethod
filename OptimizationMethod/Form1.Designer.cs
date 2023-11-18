0fea14061c3eea5f9b98f2fbcb65269896d858f9
Anastasia <anastasiya.reksius@mail.ru>
2023-11-18 16:40:23 +0700

    Добавьте файлы проекта.

---
+++ OptimizationMethod/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace OptimizationMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maximumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.XBox = new System.Windows.Forms.TextBox();
+            this.YBox = new System.Windows.Forms.TextBox();
+            this.equationBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 31);
+            this.menuStrip.TabIndex = 7;
+            this.menuStrip.Text = "menuStrip3";
+            this.menuStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip_ItemClicked);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.maximumToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(64, 27);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // maximumToolStripMenuItem
+            // 
+            this.maximumToolStripMenuItem.Name = "maximumToolStripMenuItem";
+            this.maximumToolStripMenuItem.Size = new System.Drawing.Size(176, 30);
+            this.maximumToolStripMenuItem.Text = "Рассчитать";
+            this.maximumToolStripMenuItem.Click += new System.EventHandler(this.maximumToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(176, 30);
+            this.deleteToolStripMenuItem.Text = "Очистить";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(269, 118);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(466, 300);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Результат:";
+            // 
+            // XBox
+            // 
+            this.XBox.Location = new System.Drawing.Point(75, 247);
+            this.XBox.Name = "XBox";
+            this.XBox.Size = new System.Drawing.Size(100, 22);
+            this.XBox.TabIndex = 10;
+            this.XBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // YBox
+            // 
+            this.YBox.Location = new System.Drawing.Point(75, 289);
+            this.YBox.Name = "YBox";
+            this.YBox.Size = new System.Drawing.Size(100, 22);
+            this.YBox.TabIndex = 13;
+            this.YBox.TextChanged += new System.EventHandler(this.minimumBox_TextChanged);
+            // 
+            // equationBox
+            // 
+            this.equationBox.Location = new System.Drawing.Point(368, 51);
+            this.equationBox.Name = "equationBox";
+            this.equationBox.Size = new System.Drawing.Size(252, 22);
+            this.equationBox.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(64, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "x =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "y = ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "e = ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(281, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 16);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Формула:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 16);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "x = ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 292);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 16);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "y = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.equationBox);
+            this.Controls.Add(this.YBox);
+            this.Controls.Add(this.XBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem maximumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private Label label1;
+        private TextBox XBox;
+        private TextBox YBox;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private TextBox equationBox;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label8;
+        private Label label9;
+    }
+}
+
---
+++ OptimizationMethod/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MathNet.Symbolics;
+using MathNet.Numerics;
+using MathNet.Numerics.Optimization;
+using MathNet.Numerics.Optimization.ObjectiveFunctions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Collections.Generic;
+
+namespace OptimizationMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip_ItemClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        double F(double x, double y, string formula)
+        {
+            org.matheval.Expression expression = new org.matheval.Expression(formula.ToLower());
+            expression.Bind("x", x);
+            expression.Bind("y", y);
+            double value = expression.Eval<double>();
+            return (double)value;
+        }
+
+        static double[] CoordinateDescentOptimization(Func<double, double, double> targetFunction, double initialX, double initialY, double tolerance, int maxIterations = 1000)
+        {
+            double x = initialX;
+            double y = initialY;
+
+            for (int iteration = 0; iteration < maxIterations; iteration++)
+            {
+                double oldX = x;
+                double oldY = y;
+
+                x = OptimizeCoordinate(targetFunction, x, y, "x");
+                y = OptimizeCoordinate(targetFunction, y, x, "y");
+
+                double changeX = Math.Abs(x - oldX);
+                double changeY = Math.Abs(y - oldY);
+
+                if (changeX < tolerance && changeY < tolerance)
+                {
+                    break;
+                }
+            }
+
+            return new double[] { x, y };
+        }
+
+        static double OptimizeCoordinate(Func<double, double, double> targetFunction, double coordinateValue, double otherCoordinate, string coordinateName)
+        {
+            double alpha = 0.01; // шаг обучения
+            double gradient = targetFunction(coordinateValue, otherCoordinate);
+
+            return coordinateValue - alpha * gradient;
+        }
+        private void DisplayChart(Func<double, double, double> targetFunction)
+        {
+            chart1.Series.Clear();
+
+            System.Windows.Forms.DataVisualization.Charting.Series series = new System.Windows.Forms.DataVisualization.Charting.Series();
+            series.ChartType = SeriesChartType.Line;
+
+            for (double x = -10; x <= 10; x += 0.1)
+            {
+                double y = targetFunction(x, 0); // Фиксируем одну переменную, например, y = 0
+                series.Points.AddXY(x, y);
+            }
+
+            chart1.Series.Add(series);
+            chart1.Invalidate();
+        }
+
+        private void maximumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string formula = equationBox.Text;
+            Func<double, double, double> targetFunction = (x, y) => F(x, y, formula);
+
+            double initialX = Convert.ToDouble(textBox1.Text);
+            double initialY = Convert.ToDouble(textBox2.Text);
+            double tolerance = Convert.ToDouble(textBox3.Text);
+
+           
+            double[] result = CoordinateDescentOptimization(targetFunction, initialX, initialY, tolerance);
+            XBox.Text = $"{result[0]}";
+            YBox.Text = $"{result[1]}";
+            DisplayChart(targetFunction);
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minimumBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
